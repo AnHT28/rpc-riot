bc5983d55105a35ad73408fba38c06555efb0f3d
An Hoang <anht.de@gmail.com>
2025-10-20 11:33:03 +0700

    First try with app skeleton

---
+++ app/erpc_multiply/bin/native64/cpu/native.d
@@ -0,0 +1,5 @@
+/home/an/rpc-riot/app/erpc_multiply/bin/native64/cpu/native.o: \
+ /home/an/RIOT/cpu/native/native.S /usr/include/stdc-predef.h \
+ /home/an/rpc-riot/app/erpc_multiply/bin/native64/riotbuild/riotbuild.h
+/usr/include/stdc-predef.h:
+/home/an/rpc-riot/app/erpc_multiply/bin/native64/riotbuild/riotbuild.h:
---
+++ app/erpc_multiply/main.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(void) {
+    puts("RIOT app skeleton is alive. eRPC module is linked.");
+    return 0;
+}
